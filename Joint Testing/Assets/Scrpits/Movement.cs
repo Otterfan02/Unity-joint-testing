5802a9008e5f730c43be3da3290d6f724e17064d
Yoste2904 <joakim.kristoffer.sola.strand@gmail.com>
2023-01-24 10:51:47 +0100

    Best Movement Scrpit
    Perfeksjonert movement

---
+++ Joint Testing/Assets/Scrpits/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float jumpForce;
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // right
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(speed * Time.deltaTime * Vector2.right); 
+
+        // left
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(speed * Time.deltaTime * -Vector2.right);
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.velocity = Vector2.up * jumpForce;  
+    }
+}
